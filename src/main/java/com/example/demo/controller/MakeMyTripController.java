54faac4f1d267ff70292c89a71a6daf67639591a
HarikrishnaThopugunta <harikrishnathopugunta123@gmail.com>
2023-11-20 15:42:03 +0000

    secod commit

--- src/main/java/com/example/demo/controller/MakeMyTripController.java
+++ src/main/java/com/example/demo/controller/MakeMyTripController.java
@@ -6,15 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.RequestContext;
 
 @RestController
 public class MakeMyTripController {
 	
+	@GetMapping("/")
+	public String healthCheck() {
+		return "MakeMyTrip Service {healthy:true}";
+	}
 	
 	@GetMapping("/makemytrip-train-list")
 	public ResponseEntity<Object[]> isTrainsAvaialble() {
+		System.out.println("Request Received to Make my trip app");
 		RestTemplate restTemplate = new RestTemplate();		
-		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://localhost:9090/getTrainList", Object[].class);
+		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://irctc-microservice:9090/get-train-list", Object[].class);
 		Object[] objects = responseEntity.getBody();
 		Arrays.asList(objects).forEach(System.out::println);	
 		return responseEntity;
