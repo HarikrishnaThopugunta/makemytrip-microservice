1ee04f0f2a14313810ddb2789a7402aee7933d47
HarikrishnaThopugunta <harikrishnathopugunta123@gmail.com>
2023-11-28 16:20:46 +0000

    env varibale

--- src/main/java/com/example/demo/controller/MakeMyTripController.java
+++ src/main/java/com/example/demo/controller/MakeMyTripController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,11 @@ public class MakeMyTripController {
 	@GetMapping("/makemytrip-train-list")
 	public ResponseEntity<Object[]> isTrainsAvaialble() {
 		System.out.println("Request Received to Make my trip app");
-		RestTemplate restTemplate = new RestTemplate();		
-		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("${IRCTC_MICROSERVICE_SERVICE_HOST:http://localhost}:9090/get-train-list", Object[].class);
+		RestTemplate restTemplate = new RestTemplate();	
+		String env = System.getenv("IRCTC_MICROSERVICE_SERVICE_HOST");
+		env = Objects.nonNull(env)? env :"http://localhost";
+		System.out.println("env==============================>" + env);
+		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity(env +":9090/get-train-list", Object[].class);
 		Object[] objects = responseEntity.getBody();
 		Arrays.asList(objects).forEach(System.out::println);	
 		return responseEntity;
